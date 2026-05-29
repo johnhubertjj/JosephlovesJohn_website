9fbc63cd129b6ea36f98d21ac705d61e12283964
John Hubert <jjh@vrmh.co.uk>
2026-05-29 17:43:04 +0100

    refactor: move constants out of class

--- josephlovesjohn_site/csp.py
+++ josephlovesjohn_site/csp.py
@@ -4,6 +4,26 @@ from urllib.parse import urlparse
 
 from django.conf import settings
 
+SELF_SOURCE = "'self'"
+NONE_SOURCE = "'none'"
+UNSAFE_INLINE_SOURCE = "'unsafe-inline'"
+UNSAFE_EVAL_SOURCE = "'unsafe-eval'"
+DATA_SOURCE = "data:"
+BLOB_SOURCE = "blob:"
+HTTPS_SOURCE = "https:"
+
+KIT_ORIGIN = "https://josephlovesjohn.kit.com"
+KIT_RUNTIME_ORIGIN = "https://f.convertkit.com"
+KIT_FORM_ORIGIN = "https://app.kit.com"
+STRIPE_ORIGIN = "https://checkout.stripe.com"
+RECAPTCHA_ORIGIN = "https://www.google.com"
+RECAPTCHA_STATIC_ORIGIN = "https://www.gstatic.com"
+SOUNDCLOUD_WIDGET_ORIGIN = "https://w.soundcloud.com"
+GOOGLE_FONTS_STYLES_ORIGIN = "https://fonts.googleapis.com"
+GOOGLE_FONTS_FILES_ORIGIN = "https://fonts.gstatic.com"
+META_PIXEL_SCRIPT_ORIGIN = "https://connect.facebook.net"
+META_PIXEL_EVENT_ORIGIN = "https://www.facebook.com"
+
 
 def _origin(value: str) -> str:
     """Return the scheme and host for an absolute URL."""
@@ -35,48 +55,37 @@ def build_content_security_policy() -> str:
     plausible_origin = _origin(getattr(settings, "PLAUSIBLE_SCRIPT_SRC", ""))
     public_asset_origin = _origin(getattr(settings, "PUBLIC_ASSET_BASE_URL", ""))
     media_files_origin = _origin(getattr(settings, "MEDIA_FILES_BASE_URL", ""))
-    kit_origin = "https://josephlovesjohn.kit.com"
-    kit_runtime_origin = "https://f.convertkit.com"
-    kit_form_origin = "https://app.kit.com"
-    stripe_origin = "https://checkout.stripe.com"
-    recaptcha_origin = "https://www.google.com"
-    recaptcha_static_origin = "https://www.gstatic.com"
-    soundcloud_widget_origin = "https://w.soundcloud.com"
-    google_fonts_styles_origin = "https://fonts.googleapis.com"
-    google_fonts_files_origin = "https://fonts.gstatic.com"
-    meta_pixel_script_origin = "https://connect.facebook.net"
-    meta_pixel_event_origin = "https://www.facebook.com"
     extra_sources = list(getattr(settings, "CONTENT_SECURITY_POLICY_EXTRA_SOURCES", []))
 
     directives = [
-        _directive("default-src", ["'self'"]),
-        _directive("base-uri", ["'self'"]),
-        _directive("object-src", ["'none'"]),
-        _directive("frame-ancestors", ["'none'"]),
-        _directive("form-action", ["'self'", kit_origin, kit_form_origin, stripe_origin]),
+        _directive("default-src", [SELF_SOURCE]),
+        _directive("base-uri", [SELF_SOURCE]),
+        _directive("object-src", [NONE_SOURCE]),
+        _directive("frame-ancestors", [NONE_SOURCE]),
+        _directive("form-action", [SELF_SOURCE, KIT_ORIGIN, KIT_FORM_ORIGIN, STRIPE_ORIGIN]),
         _directive(
             "script-src",
             [
-                "'self'",
-                "'unsafe-inline'",
-                "'unsafe-eval'",
+                SELF_SOURCE,
+                UNSAFE_INLINE_SOURCE,
+                UNSAFE_EVAL_SOURCE,
                 plausible_origin,
-                kit_origin,
-                kit_runtime_origin,
-                recaptcha_origin,
-                recaptcha_static_origin,
-                meta_pixel_script_origin,
+                KIT_ORIGIN,
+                KIT_RUNTIME_ORIGIN,
+                RECAPTCHA_ORIGIN,
+                RECAPTCHA_STATIC_ORIGIN,
+                META_PIXEL_SCRIPT_ORIGIN,
                 *extra_sources,
             ],
         ),
-        _directive("style-src", ["'self'", "'unsafe-inline'", google_fonts_styles_origin]),
+        _directive("style-src", [SELF_SOURCE, UNSAFE_INLINE_SOURCE, GOOGLE_FONTS_STYLES_ORIGIN]),
         _directive(
             "img-src",
             [
-                "'self'",
-                "data:",
-                "blob:",
-                "https:",
+                SELF_SOURCE,
+                DATA_SOURCE,
+                BLOB_SOURCE,
+                HTTPS_SOURCE,
                 public_asset_origin,
                 media_files_origin,
                 *extra_sources,
@@ -85,29 +94,29 @@ def build_content_security_policy() -> str:
         _directive(
             "media-src",
             [
-                "'self'",
-                "blob:",
-                "https:",
+                SELF_SOURCE,
+                BLOB_SOURCE,
+                HTTPS_SOURCE,
                 public_asset_origin,
                 media_files_origin,
                 *extra_sources,
             ],
         ),
-        _directive("font-src", ["'self'", "data:", google_fonts_files_origin]),
+        _directive("font-src", [SELF_SOURCE, DATA_SOURCE, GOOGLE_FONTS_FILES_ORIGIN]),
         _directive(
             "connect-src",
             [
-                "'self'",
+                SELF_SOURCE,
                 plausible_origin,
-                kit_origin,
-                kit_runtime_origin,
-                kit_form_origin,
-                recaptcha_origin,
-                meta_pixel_event_origin,
+                KIT_ORIGIN,
+                KIT_RUNTIME_ORIGIN,
+                KIT_FORM_ORIGIN,
+                RECAPTCHA_ORIGIN,
+                META_PIXEL_EVENT_ORIGIN,
                 *extra_sources,
             ],
         ),
-        _directive("frame-src", [kit_origin, stripe_origin, recaptcha_origin, soundcloud_widget_origin]),
+        _directive("frame-src", [KIT_ORIGIN, STRIPE_ORIGIN, RECAPTCHA_ORIGIN, SOUNDCLOUD_WIDGET_ORIGIN]),
     ]
 
     if settings.CONTENT_SECURITY_POLICY_UPGRADE_INSECURE_REQUESTS:
